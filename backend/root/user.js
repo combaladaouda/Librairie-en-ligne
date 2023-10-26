046d0c5bbb8044bf70ea3ba3fcacfc38fc6f4627
combaladaouda <combaladaouda60@gmail.com>
2023-10-26 19:31:29 +0200

    m

--- backend/root/user.js
+++ backend/root/user.js
@@ -2,7 +2,7 @@ import  express, { Router }  from "express";
 import UserControler from "../controler/user.js";
 const RouterUser = express.Router();
 
-RouterUser.use("er", UserControler.create);
+RouterUser.use("/create", UserControler.create);
 
 
 export default RouterUser;
